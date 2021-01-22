9012e8140ebb27a93462fa4ffd56d3ef16285b6d
IbrahimBurakGozen <ibrahim.gozen@student.ehb.be>
2021-01-22 04:08:09 +0100

    front-end-finished

--- front-end/signUp.js
+++ front-end/signUp.js
@@ -1,11 +1,17 @@
 'use strict';
 
+// import {
+//     resolve
+// } from "url";
+// import {
+//     rejects
+// } from "assert";
+
 
 let counter = 1;
 let userData = [];
 let userCounter = 0;
 let countForm = document.getElementById("countForm");
-let userForm;
 
 
 
@@ -17,44 +23,56 @@ countForm.addEventListener("submit", () => {
         }
     });
 
-    countForm.innerHTML = `<div class="form-group"><h1>Welcome, register all people</h1>
-    <div class="form-group">
-    <label for="exampleInputPassword1">Name person <span
-            id="numberOfPlayer">1</span></label>
-    <input type="text" class="form-control" id="namePlayer"
-        placeholder="Enter name of the player">
-    </div>
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="emailPlayer" aria-describedby="emailHelp"
-    placeholder="Enter email">
-    </div>
-    <button id="nextButtonUser" type="submit" class="btn btn-primary">Next</button>
-    <small id="emailHelp" class="form-text text-muted">Your data will not be saved, and will be
-        deleted after the experience.</small></div>`;
+    countForm.innerHTML =
+        `<div class="log-in-container">
+        <h1 class="form-title" >Welcome, register person <span
+        id="numberOfPlayer">1</span></h1>
+        <div class="form-group">
+            <label class="text-primary" for="exampleInputPassword1">Name</label>
+            <input type="text" class="form-control form-inputs" id="namePlayer"
+             placeholder="Write your name here..." required>
+        </div>
+
+        <div class="form-group">
+             <label class="text-primary" for="exampleInputEmail1">Email</label>
+            <input type="email" class="form-control form-inputs" id="emailPlayer" aria-describedby="emailHelp"
+            placeholder="Write your email here..." required>
+        </div>
+
+        <small id="emailHelp" class="form-text text-muted form-small-text">Your data will not be saved, and will be
+            deleted after the experience.</small>
+
+            <div class="form-button-container">
+                <button id="backButtonUser" type="submit" class="btn btn-primary form-next-button">Back</button>
+                <button id="nextButtonUser" type="submit" class="btn btn-primary form-next-button">Next</button>
+            </div>
+        </div>`;
 
 
     countForm.id = "userForm";
-    userForm = document.getElementById("userForm");
+    let userForm = document.getElementById("userForm");
     userForm.addEventListener("submit", dataSaveOfUser);
 });
+
 
 
 
 function dataSaveOfUser(event) {
     event.preventDefault();
     let nextButton = document.getElementById("nextButtonUser");
-    let nameInput = document.getElementById("namePlayer");
-    let emailInput = document.getElementById("emailPlayer");
+    let nameInput = document.getElementById("namePlayer").value;
+    let emailInput = document.getElementById("emailPlayer").value;
     let playerNumber = document.getElementById("numberOfPlayer");
     let maxUserCount = parseInt(userCounter, 10) + 1;
 
-    console.log(nameInput.value);
-    console.log(emailInput.value);
+
+    console.log(nameInput);
+    console.log(emailInput);
 
     userData.push({
         number: counter,
-        name: nameInput.value,
-        email: emailInput.value
+        name: nameInput,
+        email: emailInput
     });
 
     counter++;
@@ -69,8 +87,8 @@ function dataSaveOfUser(event) {
         }
 
         playerNumber.innerHTML = `${counter}`
-        nameInput.value = "";
-        emailInput.value = "";
+        nameInput = "";
+        emailInput = "";
     }
 }
 
